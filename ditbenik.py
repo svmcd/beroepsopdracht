fe037b9e960db303f6716de14abb783db00e4e2c
svmcd <spolat0750@gmail.com>
2020-09-25 14:41:35 +0200

    nieuwe bestanden

--- ditbenik.py
+++ ditbenik.py
@@ -2,7 +2,7 @@
 
 import time
 
-print("\u001b[37mHello You! Ik ben Samed")
+print("\u001b[37mHello You! Ik ben Samed, 16 jaar oud")
 
 time.sleep(1)
 print("En wie ben jij?")
@@ -35,7 +35,7 @@ answer1 = input("\u001b[37mWat is mijn favoriete kleur? \na. Rood \nb. Bosgroen 
 if answer1 == "b" or answer1 == "Bosgroen" :
     print("\u001b[32mCorrect!\u001b[37m")
     print("\n")
-else: print("\u001b[31mFout! Het antwoord was Bosgroen.\u001b[37m")
+else: print("\u001b[31mFout! Het antwoord was Bosgroen.\n\u001b[37m")
 time.sleep(2)
 
 print("Volgende vraag!\n")
@@ -45,16 +45,35 @@ answer2 = input("\u001b[37mWelke kant ga ik op? \na. Media \nb. Game \nc. Weet i
 if answer2 == "a" or answer2 == "Media" :
     print("\u001b[32mCorrect!\u001b[37m")
     print("\n")
-else: print("\u001b[31mFout! Het antwoord was Media.\u001b[37m")
+else: print("\u001b[31mFout! Het antwoord was Media.\n\u001b[37m")
 time.sleep(2)
-
-print("Laatste vraag!\n")
 
 answer3 = input("\u001b[37mWaar woon ik? \na. Amsterdam \nb. Utrecht \nc. Zaandam \nAntwoord: ")
 
 if answer3 == "c" or answer3 == "Zaandam" :
     print("\u001b[32mCorrect!\u001b[37m")
     print("\n")
-else: print("\u001b[31mFout! Het antwoord was Zaandam.\u001b[37m")
-time.sleep(2)
+else: print("\u001b[31mFout! Het antwoord was Zaandam.\n\u001b[37m")
+time.sleep(1)
+
+print("Laatste vraag!\n")
+time.sleep(1)
+
+answer4 = input("Ik twijfel tussen 2 vragen die ik aan jou wil stellen, dus ik laat jou het kiezen. Wil je een vraag over hoe oud ik ben, of een vraag over wat mijn favoriete game is? \na. Leeftijd \nb. Favoriete game\nAntwoord: ")
+
+print("\n")
+
+time.sleep(1)
+
+if answer4 == "a" :
+	answer = input("Hoe oud ben ik? \na. 17 \nb. 15 \nc. 16\nAntwoord: ")
+	if answer ==  "c" or answer == "16" : print("\u001b[32mCorrect!\u001b[37m")
+	else: print("\u001b[31mFout! Het antwoord was C.\u001b[37m")
+elif answer4 == "b" :
+	answer = input("Wat is mijn favoriete game? \na. Detroit: Become Human \nb. Gta v \nc. Minecraft\n Antwoord: ")
+	if answer == "c" or answer == "Minecraft" : print("\u001b[32mCorrect!\u001b[37m")
+	else: print("\u001b[31mFout! Het antwoord was C.\u001b[37m")
+
+print("Dit was het, hoop dat je het leuk vond!")
+	
 
